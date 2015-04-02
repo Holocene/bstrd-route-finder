3e0ae2a7c401683e8089bcc949ac1c95cca4ce1e
Holocene <a@b.com>
2015-04-02 18:46:14 +0100

    Updated config files

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 ﻿var siteConfig = {
-    firebaseUrl: 'https://wormhole-mapper.firebaseio.com/',
-    serverLoginUrl: 'https://shwc-login.azurewebsites.net/login',
+    firebaseUrl: 'https://bstrd-route-finder.firebaseio.com/',
+    serverLoginUrl: 'https://bstrd-login.azurewebsites.net/login',
     ssoHost: 'https://login.eveonline.com',
 
-	maintenanceServerUrl: 'http://shwc-maint.azurewebsites.net/',
+	maintenanceServerUrl: 'http://bstrd-maint.azurewebsites.net/',
 
     testEnvironment: false,
     type: 'prod',
-    redirectUri: 'http://holocene.github.io/shdwc-route-finder/#/login-return',
-    clientId: 'c7341d6abe5a49fa8ebe0bd5d5d43c41',
+    redirectUri: 'http://holocene.github.io/bstrd-route-finder/#/login-return',
+    clientId: '0a57577c6b344ffda738a81c01c56b4b',
 
-    homeSystemId: 30005008 
+    homeSystemId: 30002756 
 }
